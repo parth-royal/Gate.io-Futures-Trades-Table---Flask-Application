c4a8d62eb989981ed952af96f1406eee672c14d0
Your Name <you@example.com>
2023-03-11 13:56:28 +0530

    2__mar11_gateio

--- src/authm/auth.py
+++ src/authm/auth.py
@@ -5,5 +5,6 @@ def load_creds(file):
 	with open(file) as file:
 		auth = yaml.load(file,  Loader=yaml.FullLoader)
 
-	return Configuration(key=auth["gateio_api"], secret=auth["gateio_secret"])
+#	return Configuration(key=auth["gateio_api"], secret=auth["gateio_secret"])
+	return Configuration(host = "https://api.gateio.ws/api/v4", key=auth["gateio_api"], secret=auth["gateio_secret"])
 
--- src/tr.py
+++ src/tr.py
@@ -1,13 +1,25 @@
 from authm.auth import load_creds 
 from datetime import datetime
 
-from gate_api import ApiClient, Order, SpotApi
+from gate_api import ApiClient, Order, FuturesApi
 ##import load_creds func
 client = load_creds("auth/auth.yml")
-spot_api = SpotApi(ApiClient(client))
+api = FuturesApi(ApiClient(client))
 #main func
 base = "BAT"
 quote = "USDT"
-trades = spot_api.list_trades(currency_pair=f"{base}_{quote}", limit=2)
-print(trades)
+#trades = spot_api.list_trades(currency_pair=f"{base}_{quote}", limit=2)
+#print(trades)
 
+
+currency_pair = 'BAT_USDT'
+#api_response = spot_api.get_fee(currency_pair=currency_pair)
+#print(api_response)
+
+
+settle = 'usdt' 
+
+contract = 'BAT_USDT'
+
+api_response = api.get_my_trades(settle, contract=contract)
+print(api_response)
